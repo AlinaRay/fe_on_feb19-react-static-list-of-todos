df4f1b14b67e1286d5ce03d12c34d16028387191
Alina Ray <a.alina.ray@gmail.com>
2019-06-30 19:43:19 +0300

    Updates

---
+++ precache-manifest.cada54d4991b4318cf8bd6c952a032e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6763a9615e735700c3d6cfc77481de98",
+    "url": "/fe_on_feb19-react-static-list-of-todos/index.html"
+  },
+  {
+    "revision": "0432a37aca004da04da0",
+    "url": "/fe_on_feb19-react-static-list-of-todos/static/css/main.581be0ea.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/fe_on_feb19-react-static-list-of-todos/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0432a37aca004da04da0",
+    "url": "/fe_on_feb19-react-static-list-of-todos/static/js/main.8dbe1cee.chunk.js"
+  },
+  {
+    "revision": "50f9b964c5fc90f116e4",
+    "url": "/fe_on_feb19-react-static-list-of-todos/static/js/runtime~main.bd6fed63.js"
+  }
+]);
